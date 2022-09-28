a0a5ba8e12cae4e4704e4e2f1c0b9d0a06f2c4ca
ViniciusRodrigues1989 <vinicius.h.r33@gmail.com>
2022-09-28 09:20:42 -0300

    adding tests for ex 2 and 3

---
+++ src/test/java/TesteExercicio03_easy.java
@@ -0,0 +1,14 @@
+import easy.Exercicio_03_easy;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteExercicio03_easy {
+
+    @Test
+    public void testRetornaValorTrocado(){
+        Exercicio_03_easy ex03_easy = new Exercicio_03_easy();
+        String msgmEsperada = "O primeiro valor digitado é segundoValor\n" + "O segundo valor digitado é primeiroValor";
+        String msgmAtual = ex03_easy.retornaPrimeiroESegundoValor("primeiroValor", "segundoValor");
+        Assert.assertEquals("Valor diferente do Esperado", msgmEsperada,msgmAtual);
+    }
+}
