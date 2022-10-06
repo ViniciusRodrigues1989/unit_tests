79562c2dccf5a1fc6ada2e18f8b9b475b0cfd7e6
ViniciusRodrigues1989 <vinicius.h.r33@gmail.com>
2022-10-05 23:16:22 -0300

    adding exercice 10

--- src/main/java/medium/exercicio_10_while_medium.java
+++ src/main/java/medium/exercicio_10_while_medium.java
@@ -1,21 +1,28 @@
 package medium;
 
 public class exercicio_10_while_medium {
-    static int elemento,i=2;
-    public static void main(String[] args) {
-elemento =10;
-    int[] fibonacci = new int[10];
-    fibonacci[0] = 0;
-    fibonacci[1] = 1;
-    while(i < elemento){
-        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
-        i++;
-    }
-    i=0;
 
-    while (i<elemento){
-        System.out.println(fibonacci[i]);
-        i++;
-}
-}
-}
+
+        public long[] calculaTermoFibonacci(int elemento) {
+
+            int i = 2;
+            long fibonacci[] = new long[elemento];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            while (i < elemento) {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                i++;
+            }
+
+            return fibonacci;
+        }
+
+        public void imprimeVetor(long[] vetor) {
+            int i = 0;
+            while (i < vetor.length) {
+                System.out.println("Termo: " + (i + 1) + ", valor: " + vetor[i]);
+                i++;
+            }
+        }
+    }
